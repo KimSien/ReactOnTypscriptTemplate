836811b5976d86ed7d790ed515af2c134e1bd76a
kim <tksok2009@gmail.com>
2019-07-25 10:58:23 +0900

    te

--- src/js/index.tsx
+++ src/js/index.tsx
@@ -1,94 +1,54 @@
 import * as React from "react";
-import * as ReactDOM from "react-dom";
+import * as ReactDom from "react-dom";
 
+import {Provider,connect} from "react-redux";
+import {createStore,applyMiddleware} from "redux";
 
-interface BaseProps { }
-interface BaseStete { }
+import createSagaMiddleware from 'redux-saga';
+import { select,call, put, takeEvery, takeLatest ,delay ,all } from 'redux-saga/effects';
 
-/*
-class BaseApp extends React.Component<BaseProps, BaseStete>{
+const sagaMiddleware = createSagaMiddleware();
 
-    render() {
-        return (
-            <p>
-                test app
-            </p>
-        )
-    }
+function* AjaxGetPost(url: any){
+  /**
+   * ajax func
+   */
+
+   const returnValue:any = [];
+   yield put({type:"ajax_get_post",v: returnValue});
 }
-*/
 
-const style = {
-    border: '1px solid gray',
-    backgroundColor: 'white',
-  };
-  
-  interface STATES{
-    drawing :any
+function* MySaga(){
+  yield all([
+    takeLatest("AjaxGetPost",AjaxGetPost),
+  ]);
+}
+
+
+interface Aprops{}
+interface Astate{}
+class AppComponent extends React.Component<Aprops,Astate>{
+
+  state :Astate ={
+
   }
-class Canvas extends  React.Component<BaseProps, BaseStete>{
 
-    private canvas :any;
-  
-    public state: STATES;
-  
-    componentDidMount(){
-      this.canvas = React.createRef();
-  
-      this.setState({
-        drawing: false
-      });
-
-    }
-    
-    getContext() {
-      return this.canvas.getContext('2d');
-    }
-  
-    startDrawing(x :any, y :any) {
-      this.setState({ drawing: true });
-      const ctx = this.getContext();
-      ctx.moveTo(x, y);
-
-      //const ctx = this.getContext();
-      ctx.fillRect(10, 10, 100, 100);
-    }
-  
-    draw(x :any, y :any) {
-      if(!this.state.drawing){
-        return;
-      }
-      const ctx = this.getContext();
-      ctx.lineTo(x, y);
-      ctx.stroke();
-    }
-  
-    endDrawing() {
-      this.setState({ drawing: false });
-    }
-    
-    //var canvas = <HTMLCanvasElement> document.getElementById('lineChartCanvas');
-    //var ctx = canvas.getContext("2d");
-  
-    render() {
-      return (
-        <canvas
-          ref ={(self) => { this.canvas = self; }}
-          width="500px"
-          height="500px"
-          onMouseDown={e => this.startDrawing(e.nativeEvent.offsetX, e.nativeEvent.offsetY)}
-          onMouseUp={() => this.endDrawing()}
-          onMouseLeave={() => this.endDrawing()}
-          onMouseMove={e => this.draw(e.nativeEvent.offsetX, e.nativeEvent.offsetY)}
-          style={style}
-        />
-      );
-    }
-
+  public render(){
+    return(
+      <p>test</p>
+    );
+  }
 }
 
-ReactDOM.render(
-    <Canvas />
-    ,
-    document.getElementById('app')
-)
+const App :any =(()=>{
+
+});
+
+
+ReactDom.render(
+
+  <App />
+  ,
+  document.getElementById("app")
+
+);
